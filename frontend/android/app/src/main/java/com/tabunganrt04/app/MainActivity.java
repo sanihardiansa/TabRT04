5ceaca1de0b997b6d6adecb4e904db9bb87b030a
sanihardiansa <sanihard@gmail.com>
2026-07-02 11:46:44 +0700

    feat: setup capacitor and config for android apk compilation

---
+++ frontend/android/app/src/main/java/com/tabunganrt04/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tabunganrt04.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,6 +18,6 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    minify: 'terser',
+    minify: 'esbuild',
   }
 });
